610a525408fec0f752848685987b9ad3662ee565
Anish Chaudhary <anishcdy584@gmail.com>
2025-11-03 07:34:25 +0545

    first commit

--- server/models/Room.js
+++ server/models/Room.js
@@ -5,7 +5,7 @@ const roomSchema = new mongoose.Schema(
     hotel: { type: String, ref: "Hotel", required: true },
     roomType: { type: String, required: true },
     pricePerNight: { type: Number, required: true },
-    amentities: { type: Array, required: true },
+    amenities: { type: [String], required: true },
     images: [{ type: String }],
     isAvailable: { type: Boolean, default: true },
   },
